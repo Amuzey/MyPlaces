fb10211fd20c71b32e641a6b321eafbf41a731b8
Amuzey <ivanovad97@gmail.com>
2022-08-29 14:42:55 +0500

    Image Picker Controller Design for New Place View Controller

--- MyPlaces/ViewControllers/PlacesTableViewController.swift
+++ MyPlaces/ViewControllers/PlacesTableViewController.swift
@@ -33,21 +33,6 @@ class PlacesTableViewController: UITableViewController {
         
         return cell
     }
-    
-    // MARK: - Table view Delegate
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        85
-    }
 }
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
 
 
