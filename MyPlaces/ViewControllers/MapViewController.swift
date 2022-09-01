a6a6aad4abe1928fad4b9a58cf0479dab548ed68
Amuzey <ivanovad97@gmail.com>
2022-09-01 19:07:35 +0500

    work with annotations

--- MyPlaces/ViewControllers/MapViewController.swift
+++ MyPlaces/ViewControllers/MapViewController.swift
@@ -15,14 +15,16 @@ class MapViewController: UIViewController {
     
     // MARK: - Public properties
     var place: Place!
+    let annotationIdentifier = "annotationIdentifier"
     
     // MARK: - Lyfe Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         setupPlacemark()
     }
     
-
+    
     
     // MARK: - IBActions
     @IBAction func cancelAction() {
@@ -53,5 +55,30 @@ class MapViewController: UIViewController {
             self.mapView.selectAnnotation(annotation, animated: true)
             
         }
+    }
+}
+
+// MARK: - MKMap View Delegate
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        if let imageData = place.imageData {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        
+        
+        return annotationView
+        
     }
 }
